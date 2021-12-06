895efe179a5883545ef980a086a87302a694e919
MaxLyonel <leorexx100@gmail.com>
2021-12-06 00:38:16 -0400

    tercera pregunta segundo examen de 324

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.RECUPERAR = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Mostrar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Almacenar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 461);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // RECUPERAR
+            // 
+            this.RECUPERAR.Location = new System.Drawing.Point(438, 24);
+            this.RECUPERAR.Name = "RECUPERAR";
+            this.RECUPERAR.Size = new System.Drawing.Size(114, 23);
+            this.RECUPERAR.TabIndex = 1;
+            this.RECUPERAR.Text = "Recuperar";
+            this.RECUPERAR.UseVisualStyleBackColor = true;
+            this.RECUPERAR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(475, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(475, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(475, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(77, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(451, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(451, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "G:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(451, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(582, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(394, 461);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Mostrar
+            // 
+            this.Mostrar.Location = new System.Drawing.Point(438, 284);
+            this.Mostrar.Name = "Mostrar";
+            this.Mostrar.Size = new System.Drawing.Size(114, 23);
+            this.Mostrar.TabIndex = 15;
+            this.Mostrar.Text = "Mostrar";
+            this.Mostrar.UseVisualStyleBackColor = true;
+            this.Mostrar.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ROJO",
+            "AZUL",
+            "VERDE",
+            "AMARILLO",
+            "ROSADO",
+            "NARANJA"});
+            this.comboBox1.Location = new System.Drawing.Point(438, 188);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 21);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.Text = "ROJO";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Almacenar
+            // 
+            this.Almacenar.Location = new System.Drawing.Point(438, 234);
+            this.Almacenar.Name = "Almacenar";
+            this.Almacenar.Size = new System.Drawing.Size(114, 23);
+            this.Almacenar.TabIndex = 17;
+            this.Almacenar.Text = "Almacenar";
+            this.Almacenar.UseVisualStyleBackColor = true;
+            this.Almacenar.Click += new System.EventHandler(this.Almacenar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 489);
+            this.Controls.Add(this.Almacenar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Mostrar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RECUPERAR);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.button7_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button RECUPERAR;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Mostrar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Almacenar;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public int id = 0, cR, cB, cG;
+        public Boolean sw = true;
+        public Bitmap bmp = null, copia=null, cop = null;
+        public String color = String.Empty;
+        SqlConnection con;
+        public string conString = "Data Source=DESKTOP-U1FMLGK\\SQLEXPRESS;Initial Catalog=SegundoExamen324;Integrated Security=True; MultipleActiveResultSets=true";
+        public Form1()
+        {
+            InitializeComponent();
+            conexion();
+        }
+
+        public void conexion()
+        {
+            con = new SqlConnection(); // Un objeto que nos ayuda a hacer la conexión
+            //SqlDataAdapter ada = new SqlDataAdapter();
+            //DataSet ds = new DataSet();
+            con.ConnectionString = conString; // acá se manda la cadena de conexión
+        
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+                Console.WriteLine("Conexión con SQL-Server exitosa!");
+            else
+                Console.WriteLine("No se pudo conectar a la BD");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {           
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            int x, y, mR = 0, mG = 0, mB = 0;
+            x = e.X;
+            y = e.Y;
+            for (int i = x; i < x + 10; i++)
+            {
+                for (int j = y; j < y + 10; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    mR = mR + c.R;
+                    mG = mG + c.G;
+                    mB = mB + c.B;
+                }
+            }
+            mR = mR / 100;
+            mG = mG / 100;
+            mB = mB / 100;
+            cR = mR;
+            cG = mG;
+            cB = mB;
+            
+            textBox1.Text = cR.ToString();
+            textBox2.Text = cG.ToString();
+            textBox3.Text = cB.ToString();
+            
+        }
+        // Cambio click 2
+        private void button7_Click(object sender, EventArgs e)
+        {                        
+            int meR, meG, meB;
+            if (sw)
+            {
+                Console.WriteLine("primera vez");
+                bmp = new Bitmap(pictureBox1.Image);
+                copia = new Bitmap(bmp.Width, bmp.Height);
+                sw = false;
+            }
+            Color c = new Color();
+            // Acá se realiza la consulta
+            SqlCommand sel = new SqlCommand();
+            sel.Connection = con;
+            sel.CommandText = @"
+                SELECT color, R,G,B FROM TEXTURAS  
+            ";
+            try
+            {
+                SqlDataReader dr = sel.ExecuteReader();
+                // Esto va iterar la cantidade de datos recuperados
+                while (dr.Read())
+                {
+                    // Por cada iteración se va a obtener su color y sus color de imagen
+                    string col = dr["color"].ToString();
+                    int aR = (int)dr["R"];
+                    int aG = (int)dr["G"];
+                    int aB = (int)dr["B"];
+                    
+                    // Acá se recorre toda la imagen
+                    for (int i = 0; i < bmp.Width - 10; i += 10)
+                    {
+                        for (int j = 0; j < bmp.Height - 10; j += 10)
+                        {
+                            meR = 0;
+                            meG = 0;
+                            meB = 0;
+                            for (int k = i; k < i + 10; k++)
+                                for (int l = j; l < j + 10; l++)
+                                {
+                                    c = bmp.GetPixel(k, l);
+                                    meR = meR + c.R;
+                                    meG = meG + c.G;
+                                    meB = meB + c.B;
+                                }
+                            meR = meR / 100;
+                            meG = meG / 100;
+                            meB = meB / 100;
+                            
+                            if ((aR - 10 < meR) && (meR < aR + 10) && (aG - 10 < meG) && (meG < aG + 10) && (aB - 10 < meB) && (meB < aB + 10))
+                            {
+                                // Acá se va pintando los píxeles 
+                                for (int k = i; k < i + 10; k++)
+                                    for (int l = j; l < j + 10; l++)
+                                    {
+                                        switch (color)
+                                        {
+                                            case "ROJO":
+                                                copia.SetPixel(k, l, Color.Red); 
+                                                break;
+                                            case "AZUL":
+                                                copia.SetPixel(k, l, Color.Blue);
+                                                break;
+                                            case "VERDE":
+                                                copia.SetPixel(k, l, Color.Green);
+                                                break;
+                                            case "AMARILLO":
+                                                copia.SetPixel(k, l, Color.Yellow);
+                                                break;
+                                            case "NARANJA":
+                                                copia.SetPixel(k, l, Color.Orange);
+                                                break;
+                                            case "ROSADO":
+                                                copia.SetPixel(k, l, Color.Pink);
+                                                break;
+                                        }                                        
+                                    }                        
+                            }
+                            else
+                            {
+                                for (int k = i; k < i + 10; k++)
+                                    for (int l = j; l < j + 10; l++)
+                                    {
+                                        c = bmp.GetPixel(k, l);
+                                        
+                                        copia.SetPixel(k, l, c);                                
+                                    }                        
+                            }
+                        }
+                    }
+                    bmp = copia; // Esto soluciono parte del problema                                        
+                }
+                pictureBox2.Image = copia;                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = comboBox1.SelectedIndex;
+            color = comboBox1.Items[indice].ToString();
+            //Console.WriteLine(color);
+        }
+
+        private void Almacenar_Click(object sender, EventArgs e)
+        {
+            // aca se tiene que ejecutar la instrucción de insert
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            // Enviando parámetros a la consulta
+            cmd.Parameters.Add("@ID", SqlDbType.Int);
+            cmd.Parameters["@ID"].Value = id;
+            cmd.Parameters.AddWithValue("@color", color); // hay que determinar el color de inserción en la base de datos
+            cmd.Parameters.AddWithValue("@R", cR);
+            cmd.Parameters.AddWithValue("@G", cG);
+            cmd.Parameters.AddWithValue("@B", cB);
+            cmd.CommandText =
+                "Insert Into TEXTURAS (codigo, color, R,G,B)" +
+                " Values (@ID, @color, @R, @G, @B)";                       
+            try
+            {               
+                Int32 rowsAffected = cmd.ExecuteNonQuery();
+                Console.WriteLine("RowsAffected: {0}", rowsAffected);
+                id++;
+                textBox1.Text = String.Empty;
+                textBox2.Text = String.Empty;
+                textBox3.Text = String.Empty;
+                
+            }
+            catch (Exception ex)
+            {               
+                Console.WriteLine("Quizá, no llenaste datos");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Mostrar_Texturas(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
